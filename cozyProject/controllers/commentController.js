df92185c00e3a51dfa90604fb343d2b900eaea58
rkdls8268 <rkdls8268@gmail.com>
2020-09-14 01:26:45 +0900

    modify: comment 관련

--- cozyProject/controllers/commentController.js
+++ cozyProject/controllers/commentController.js
@@ -1,4 +1,5 @@
 const CommentModel = require('../models/commentModel');
+const ActivityModel = require('../models/activityModel');
 const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/resMessage');
 const util = require('../modules/util');
@@ -19,8 +20,8 @@ const comment = {
                         commentIdx: 0,
                         userIdx: nickname[0].userIdx,
                         bookstoreIdx: 0,
-                        content: 'NULL',
-                        createdAt: 'NULL',
+                        content: null,
+                        createdAt: null,
                         nickname: nickname[0].nickname
                     }));
                 }else{
@@ -43,8 +44,20 @@ const comment = {
                 if (!ActivityIdx || !content) {
                     return res.status(statusCode.OK).send(util.fail(statusCode.OK, resMessage.NULL_VALUE));
                 }
+            
+                // TODO: activityIdx 가 활동테이블에서 존재하지 않을 때 fail 처리. 굳이 안해줘도 될 것 같은데 혹시 몰라서 해줌..
+                const activityResult = await ActivityModel.isActivityRegistered(ActivityIdx);
+                console.log(activityResult);
+                if (activityResult.length === 0) {
+                    return res.status(statusCode.OK).send(util.fail(statusCode.OK, resMessage.NO_ACT_DATA));
+                }
+
+                // 우선 insert 이후 response body 로 필요한 값 없을 것 같아서 commentIdx 만 보내줌. 
                 const result = await CommentModel.writeComment(userIdx, ActivityIdx, content);
-                module.exports.showAllCommentsOfActivity(req, res);
+                res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.INSERT_COMMENT_SUCCESS, {
+                    commentIdx: result
+                }));
+                // module.exports.showAllCommentsOfActivity(req, res);
             }catch(err){
                 res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
             }
@@ -84,6 +97,28 @@ const comment = {
                     res.status(statusCode.OK).send(util.fail(statusCode.OK, resMessage.ERROR_IN_DELETE_COMMENT));
                 }
             }catch(err){
+                res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
+            }
+        }
+    },
+    showCheckedComment: async (req, res) => {
+        const commentIdx = req.params.commentIdx;
+        if (req.decoded === undefined) {
+            return res.status(statusCode.OK).send(util.fail(statusCode.OK, resMessage.EMPTY_TOKEN));
+        } else {
+            const result = await CommentModel.checkComment(commentIdx);
+            console.log(result);
+            try {
+                if (result.length === 0) {
+                    return res.status(statusCode.OK).send(util.fail(statusCode.OK, resMessage.NO_COMMENT));
+                }
+                else return res.status(statusCode.OK).send(util.success(statusCode.OK, resMessage.SELECT_COMMENT, {
+                    commentIdx: result[0].commentIdx,
+                    userIdx: result[0].userIdx,
+                    activityIdx: result[0].activityIdx,
+                    content: result[0].content
+                }));
+            } catch (err) {
                 res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
             }
         }
